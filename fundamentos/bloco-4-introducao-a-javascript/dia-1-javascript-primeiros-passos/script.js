bcb13e853676fc7ad6011da8d0bb2110731f7c9d
Gabriel Grippa Alves <gabrielgrippa1@gmail.com>
2021-11-26 19:44:57 -0300

    adiciona exercicios do dia 4.1

---
+++ fundamentos/bloco-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,47 @@
+// Exercicio 1
+// let a = 5
+// let b = 7
+// let c = 10
+// let add = (a+b)
+// let subt = (a-b)
+// let mult = (a*b)
+// let division = (a/b)
+// let rest = (a%b)
+
+// // Exercicio 2
+// if (a>b) {console.log ('a é maior')}
+// else if (a<b) {console.log ('b é maior')}
+// else {console.log ('a e b são iguais')}
+
+// //exercicio 3
+// if (a>b && a>c) {console.log ('a')}
+// else if (b>a && b>c) {console.log ('b')}
+// else if (c>a && c>b) {console.log ('c')}
+
+// //exercicio 4
+// if (a>0) {console.log ('positive')}
+// else if (a<0) {console.log ('negative')}
+// else {console.log ('zero')}
+
+//exercicio 5
+// const ang1 = 60
+// const ang2 = 40
+// const ang3 = 80
+// if (ang1 + ang2 + ang3 == 180 ) {return true}
+// else if (ang1 + ang2 + ang3 != 180 && ang1 + ang2 + ang3>0) {return false}
+// else {return 'error'}
+
+//exercicio 6
+
+function movement (chessPiece){
+    if (chessPiece.toLowerCase() == "king") {console.log ('movimentos do rei')} 
+    else if (chessPiece.toLowerCase() == "queen") {console.log ('movimentos da rainha')} 
+    else if (chessPiece.toLowerCase() == "rook") {console.log ('movimentos da torre')} 
+    else if (chessPiece.toLowerCase() == "bishop") {console.log ('movimentos do bispo')} 
+    else if (chessPiece.toLowerCase() == "knight") {console.log ('movimentos do cavalo')} 
+    else if (chessPiece.toLowerCase() == "pawn") {console.log ('movimentos do peão')} 
+    else {console.log ("error")}
+}
+
+function ("QUEEN")
+
