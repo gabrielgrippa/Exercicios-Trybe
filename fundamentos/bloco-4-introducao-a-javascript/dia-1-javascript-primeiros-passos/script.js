a973986321efe794c40687dc9b312ac42cbef520
Gabriel Grippa Alves <gabrielgrippa1@gmail.com>
2021-11-27 19:43:24 -0300

    finaliza todos exercicios

--- fundamentos/bloco-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -33,15 +33,54 @@
 
 //exercicio 6
 
-function movement (chessPiece){
-    if (chessPiece.toLowerCase() == "king") {console.log ('movimentos do rei')} 
-    else if (chessPiece.toLowerCase() == "queen") {console.log ('movimentos da rainha')} 
-    else if (chessPiece.toLowerCase() == "rook") {console.log ('movimentos da torre')} 
-    else if (chessPiece.toLowerCase() == "bishop") {console.log ('movimentos do bispo')} 
-    else if (chessPiece.toLowerCase() == "knight") {console.log ('movimentos do cavalo')} 
-    else if (chessPiece.toLowerCase() == "pawn") {console.log ('movimentos do peão')} 
-    else {console.log ("error")}
-}
+// function movement (chessPiece){
+//     if (chessPiece.toLowerCase() == "king") {console.log ('movimentos do rei')} 
+//     else if (chessPiece.toLowerCase() == "queen") {console.log ('movimentos da rainha')} 
+//     else if (chessPiece.toLowerCase() == "rook") {console.log ('movimentos da torre')} 
+//     else if (chessPiece.toLowerCase() == "bishop") {console.log ('movimentos do bispo')} 
+//     else if (chessPiece.toLowerCase() == "knight") {console.log ('movimentos do cavalo')} 
+//     else if (chessPiece.toLowerCase() == "pawn") {console.log ('movimentos do peão')} 
+//     else {console.log ("error")}
+// }
 
-function ("QUEEN")
+//exercicio 7
 
+//exercicio 8
+// let num1 
+// let num2
+// let num3
+// if (num1%2==0 || num2%2==0 || num3%2==0){
+//     console.log (true)}
+// else {console.log (false)}
+
+//exercicio 9
+
+//exercicio 10
+// const cost = 5
+// const sell = 10
+ 
+// function profitCalc1000 (cost, sell) {
+//     let totalCost = cost + (cost/5)
+//     let profit = sell - totalCost
+//     if (cost < 0 || sell < 0) {
+//         console.log("Error")
+//     } 
+//     else {console.log (profit*1000)}
+// }
+// profitCalc1000 (cost, sell)
+
+//exercicio 11
+// let bruteSal
+// function salaryCalc (bruteSal){
+//     if (bruteSal < 1556.95){baseSal = bruteSal - ((8/100)*bruteSal)}
+//     else if (bruteSal >= 1556.95 && bruteSal <= 2594.92){baseSal = bruteSal - ((9/100)*bruteSal)}
+//     else if (bruteSal >= 2594.93 && bruteSal <= 5189.82){baseSal = bruteSal - ((11/100)*bruteSal)}
+//     else if (bruteSal > 5189.82){baseSal = bruteSal - 570.88}
+
+//     if (baseSal <= 1903.98){finalSal = baseSal}
+//     else if (baseSal >= 1903.99 && baseSal <=2826.65){finalSal = baseSal - ((7.5/100) * baseSal) + 142.80}
+//     else if (baseSal >= 2826.66 && baseSal <=3751.05){finalSal = baseSal - ((15/100) * baseSal) + 354.80}
+//     else if (baseSal >= 3751.06 && baseSal <=4664.68){finalSal = baseSal - ((22.5/100) * baseSal) + 636.13}
+//     else if (baseSal >= 4664.68){finalSal = baseSal - ((27.5/100) * baseSal) + 869.36}
+//     console.log (finalSal)
+//     }
