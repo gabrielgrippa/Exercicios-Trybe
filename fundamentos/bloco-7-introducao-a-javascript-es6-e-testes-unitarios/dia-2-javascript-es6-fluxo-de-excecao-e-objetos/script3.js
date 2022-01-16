baf9d045fef9a559e3b5095d5256572a7b8f0d0f
Gabriel Grippa Alves <gabrielgrippa1@gmail.com>
2022-01-16 20:31:02 -0300

    exercicios dia 7.2

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script3.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+function addEntry(object, key, value){
+  let newKey = key
+  object[newKey] = value
+  console.log(object)
+}
+addEntry(lesson2, 'turno', 'noite')
